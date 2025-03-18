cb6b78a3212564dfa795f36ce831340ebd2c4532
Teddy Levante <levantea@sheridancollege.ca>
2025-03-18 17:29:11 -0400

    week 10 coding gym log 2: completed task 2 of the turn based battler banana will now perform the animation on button press button is non-interactable while coroutine is running

--- Assets/Scripts/Week10/codingGym/bananaGrow.cs
+++ Assets/Scripts/Week10/codingGym/bananaGrow.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class bananaGrow : MonoBehaviour
 {
     public Transform banana;
     public AnimationCurve curve;
     public float t;
+    public Button attackButton;
 
-    // Start is called before the first frame update
-    void Start()
+    
+    public void bananamationStart()
     {
         StartCoroutine(bananamation());
     }
 
     public IEnumerator bananamation()
     {
+        attackButton.interactable = false;
         t = 0.5f;
         while(t < 1)
         {
@@ -30,6 +33,6 @@ public class bananaGrow : MonoBehaviour
             banana.transform.localScale = (Vector3.one * t);
             yield return null;
         }
-        
+        attackButton.interactable = true;
     }
 }
